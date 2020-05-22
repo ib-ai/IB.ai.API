d16671d4e584c6cacc9eec6b4d86105513cb6ba0
Nathaneal Varghese <nathaneal.varghese@gihs.sa.edu.au>
2020-05-22 13:52:23 +0930

    Cleaned up code in Tags

--- front/src/components/sideBarSections/Tags.js
+++ front/src/components/sideBarSections/Tags.js
@@ -29,11 +29,6 @@ const useStyles = makeStyles({
   root: {
     minWidth: 275,
   },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
-  },
   title: {
     fontSize: 14,
   },
