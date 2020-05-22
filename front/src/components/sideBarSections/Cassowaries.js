7a56157505a4d4fc3266620f08a71f6894944e74
Nathaneal Varghese <nathaneal.varghese@gihs.sa.edu.au>
2020-05-22 13:52:47 +0930

    Started working on Cassowaries Card

--- front/src/components/sideBarSections/Cassowaries.js
+++ front/src/components/sideBarSections/Cassowaries.js
@@ -1,13 +1,36 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import Grid from "@material-ui/core/Grid";
+import { makeStyles, ThemeProvider } from "@material-ui/core/styles";
+import { createMuiTheme } from "@material-ui/core/styles";
 
-const useStyles = makeStyles({
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Grid from "@material-ui/core/Grid";
+import MaterialTable from "material-table";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import Chip from "@material-ui/core/Chip";
+
+import Search from "@material-ui/icons/Search";
+import ViewColumn from "@material-ui/icons/ViewColumn";
+import SaveAlt from "@material-ui/icons/SaveAlt";
+import ChevronLeft from "@material-ui/icons/ChevronLeft";
+import ChevronRight from "@material-ui/icons/ChevronRight";
+import FirstPage from "@material-ui/icons/FirstPage";
+import LastPage from "@material-ui/icons/LastPage";
+import Add from "@material-ui/icons/Add";
+import Check from "@material-ui/icons/Check";
+import FilterList from "@material-ui/icons/FilterList";
+import Remove from "@material-ui/icons/Remove";
+import Edit from "@material-ui/icons/Edit";
+import Delete from "@material-ui/icons/Delete";
+import Clear from "@material-ui/icons/Clear";
+import Cancel from "@material-ui/icons/Cancel";
+import { Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
   root: {
     minWidth: 275,
   },
@@ -28,39 +51,184 @@ const useStyles = makeStyles({
     marginLeft: "20px",
     width: "100%",
   },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+    maxWidth: 300,
+  },
+  chips: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  chip: {
+    margin: 2,
+  },
+  noLabel: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+const theme = createMuiTheme({
+  cardTypography: {
+    fontSize: 13,
+  },
 });
+
+const allRoleIds = [
+  "0396839046893046",
+  "25982385825833535",
+  "20935823950298503",
+];
 
 function Cassowaries() {
   const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
+
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
+
+  function getStyles(id, roleId, theme) {
+    return {
+      fontWeight:
+        roleId.indexOf(id) === -1
+          ? theme.typography.fontWeightRegular
+          : theme.typography.fontWeightMedium,
+    };
+  }
+
+  const [roleIds, setRoleIds] = React.useState([]);
+
+  function handleChange(event) {
+    setRoleIds(event.target.value);
+  }
+
+  const [cassowaries, setCassowaries] = React.useState({
+    columns: [
+      { title: "Cassowary Name", field: "name" },
+      {
+        title: "Cassowary Roles",
+        field: "roles",
+        render: (rowData) => {
+          return (
+            <li>
+              {rowData.roles.map((role) => (
+                <Chip label={role} className={classes.chip} />
+              ))}
+            </li>
+          );
+        },
+        editComponent: (props) => (
+          <FormControl className={classes.formControl}>
+            <InputLabel>Roles</InputLabel>
+            <Select
+              multiple
+              value={roleIds}
+              onChange={handleChange}
+              input={<Input id="select-multiple-chip" />}
+              renderValue={(selected) => {
+                console.log(selected);
+
+                return (
+                  <div className={classes.chips}>
+                    {selected.map((value) => (
+                      <Chip
+                        key={value}
+                        label={value}
+                        className={classes.chip}
+                      />
+                    ))}
+                  </div>
+                );
+              }}
+              // MenuProps={MenuProps}
+            >
+              {allRoleIds.map((id) => (
+                <MenuItem key={id} value={id}>
+                  {id}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        ),
+      },
+      { title: "Penguin", field: "penguin" },
+    ],
+    data: [{ name: "Mehmet", roles: roleIds, penguin: true }],
+  });
 
   return (
     <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
       <div className={classes.root}>
         <Card className={classes.cardDesign}>
           <CardContent>
-            <Typography
-              className={classes.title}
-              color="textSecondary"
-              gutterBottom
-            >
-              Cassowaries
-            </Typography>
-            <Typography variant="h5" component="h2">
-              be{bull}nev{bull}o{bull}lent
-            </Typography>
-            <Typography className={classes.pos} color="textSecondary">
-              adjective
-            </Typography>
-            <Typography variant="body2" component="p">
-              well meaning and kindly.
-              <br />
-              {'"a benevolent smile"'}
-            </Typography>
+            <MaterialTable
+              icons={{
+                Check: Check,
+                DetailPanel: ChevronRight,
+                Export: SaveAlt,
+                Filter: FilterList,
+                FirstPage: FirstPage,
+                LastPage: LastPage,
+                NextPage: ChevronRight,
+                PreviousPage: ChevronLeft,
+                Search: Search,
+                ThirdStateCheck: Remove,
+                Add: Add,
+                ViewColumn: ViewColumn,
+                Edit: Edit,
+                Delete: Delete,
+                Clear: Clear,
+                ResetSearch: Cancel,
+              }}
+              title="Cassowary List"
+              columns={cassowaries.columns}
+              data={cassowaries.data}
+              editable={{
+                onRowAdd: (newData) =>
+                  new Promise((resolve) => {
+                    setTimeout(() => {
+                      resolve();
+                      setCassowaries((prevState) => {
+                        const data = [...prevState.data];
+                        data.push(newData);
+                        return { ...prevState, data };
+                      });
+                    }, 600);
+                  }),
+                onRowUpdate: (newData, oldData) =>
+                  new Promise((resolve) => {
+                    setTimeout(() => {
+                      resolve();
+                      if (oldData) {
+                        setCassowaries((prevState) => {
+                          const data = [...prevState.data];
+                          data[data.indexOf(oldData)] = newData;
+                          return { ...prevState, data };
+                        });
+                      }
+                    }, 600);
+                  }),
+                onRowDelete: (oldData) =>
+                  new Promise((resolve) => {
+                    setTimeout(() => {
+                      resolve();
+                      setCassowaries((prevState) => {
+                        const data = [...prevState.data];
+                        data.splice(data.indexOf(oldData), 1);
+                        return { ...prevState, data };
+                      });
+                    }, 600);
+                  }),
+              }}
+            />
           </CardContent>
-          <CardActions>
-            <Button size="small">Learn More</Button>
-          </CardActions>
         </Card>
       </div>
     </Grid>
